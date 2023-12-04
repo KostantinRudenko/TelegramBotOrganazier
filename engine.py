798cb617caf46544fdc0adb56eecab2aa24e49e8
Rudenko_Kostya2010 <rudenkokostya10@gmail.com>
2023-12-04 15:34:27 +0200

    fixed get_time function

--- engine.py
+++ engine.py
@@ -13,8 +13,8 @@ class Engine:
         hour_time = date_data.hour
         
         if hour_time <= 12:
-            date = f"East:\n\t{date_data.day}-{date_data.month}-{date_data.year}\n\t{date_data.hour}:{date_data.minute}\nEast:\n\t{date_data.month}-{date_data.day}-{date_data.year}\n\t{date_data.hour}a.m.:{date_data.minute}"
+            date = f"East:\n\t{date_data.day}-{date_data.month}-{date_data.year}\n\t{date_data.hour}:{date_data.minute}\nWest:\n\t{date_data.month}-{date_data.day}-{date_data.year}\n\t{date_data.hour}a.m.:{date_data.minute}"
         else:
-            date = f"West:\n\t{date_data.month}-{date_data.day}-{date_data.year}\n\t{date_data.hour-12}p.m.:{date_data.minute}"
+            date = f"East:\n\t{date_data.day}-{date_data.month}-{date_data.year}\n\t{date_data.hour}:{date_data.minute}\nWest:\n\t{date_data.month}-{date_data.day}-{date_data.year}\n\t{date_data.hour-12}p.m.:{date_data.minute}"
         
         return date
